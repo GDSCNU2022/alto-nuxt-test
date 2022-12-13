e9d89dfee9b9f7e879009082a8a11981bbd423f9
iwamamalto <csar21007@g.nihon-u.ac.jp>
2022-12-13 14:49:05 +0900

    test commit

---
+++ plugins/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// .envからプロジェクトIDを取得して定数に設定
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.nuxt-test-afd7b,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MESUREMENT_ID
+}
+
+// firebaseの初期化処理
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export default firebase
